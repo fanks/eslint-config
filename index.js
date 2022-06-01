40b9b5dd6d3acf8eafc5a38eaf5da3798ea9934e
Magnus Lind <fankso@gmail.com>
2022-06-01 18:43:45 +0200

    tweaks

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const base = require("./rules/base.js")
+const rule_import = require("./lib/import")
+const rule_ts = require("./lib/typescript")
+const rule_prettier = require("./lib/prettier")
 
 module.exports = {
+  env: { jest: true, browser: true, es6: true, node: true },
   extends: [
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "plugin:prettier/recommended"
   ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["log", "@typescript-eslint", "import"],
   rules: {
-    ...base
-  }
+    "prettier/prettier": ["error", rule_prettier],
+    ...rule_ts,
+    ...rule_import
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/"]
+      }
+    }
+  },
+  ignorePatterns: ["node_modules/", "dist/", "build/"]
 }
